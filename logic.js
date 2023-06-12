184bcef4acd357c792835f0da989bcebd2af8129
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-06-12 23:30:23 +0530

    Added:Input Form

---
+++ logic.js
@@ -0,0 +1,61 @@
+// const text = document.querySelector('#text');
+// const textError = document.querySelector('.text-error');
+// text.addEventListener('input', function () {
+//     let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+//     if (nameRegex.test(text.value))
+//         textError.textContent = "";
+//     else textError.textContent = "Name is Incorrect";
+// });
+
+
+
+// const text = document.querySelector('#text');
+// const textError = document.querySelector('.text-error');
+
+// text.addEventListener('blur', function () {
+//     let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+//     if (nameRegex.test(text.value)) {
+//         textError.textContent = "";
+//     } else {
+//         textError.textContent = "Name is Incorrect";
+//     }
+// });
+
+const text = document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function() {
+  let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+  if (nameRegex.test(text.value)) {
+    textError.textContent = "";
+  } else {
+    textError.textContent = "Name is incorrect";
+  }
+});
+
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input',function(){
+    let emailRegex = RegExp('^[0-9a-zA-Z]+([+.-]([a-z0-9A-Z]+))*[@][a-zA-Z0-9]+[.][a-z]{2,4}[,]?([.][a-z]{2,4})?$');
+    if(emailRegex.test(email.value))
+    emailError.textContent="";
+    else emailError.textContent="Not a valid Email"
+})
+
+const number = document.querySelector('#number')
+const numberError = document.querySelector('.number-error')
+number.addEventListener('input',function(){
+    let numberRegex = RegExp('^[0-9]{2,}[0-9]{10,}$')
+    if(numberRegex.test(number.value))
+    numberError.textContent="";
+    else numberError.textContent="Check your phone number"
+})
+
+const pwd = document.querySelector('#pwd');
+const pwdError = document.querySelector('.pwd-error');
+pwd.addEventListener('input', function() {
+let pass = RegExp('^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+])[A-Za-z\\d!@#$%^&*()_+]{8,}$');
+if (pass.test(pwd.value))
+    pwdError.textContent = "";
+  else
+    pwdError.textContent = "Wrong password";
+});
